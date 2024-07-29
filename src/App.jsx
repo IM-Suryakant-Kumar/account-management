633cbe361461eb44305e448d2b2cf627258679a3
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-07-29 18:56:47 +0530

    feat: Added Layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { Fragment } from "react";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import { Layout, HostLayout, AuthLayout } from "./components";
+import { Home, Login, Signup, NotFound } from "./pages";
 
 function App() {
-	return <Fragment></Fragment>;
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<Layout />}>
+				<Route element={<HostLayout />}>
+					<Route index element={<Home />} />
+				</Route>
+				<Route element={<AuthLayout />}>
+					<Route path="login" element={<Login />} />
+					<Route path="signup" element={<Signup />} />
+				</Route>
+				<Route path="*" element={<NotFound />} />
+			</Route>
+		)
+	);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
